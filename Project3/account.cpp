93921ae1792682e0622d32dde60246ae96aadc33
tw2013.kim <tigger6392@gmail.com>
2023-06-01 14:10:59 +0900

    Add additional operations for interest
    Signed-off-by: tw2013.kim <tigger6392@gmail.com>

--- Project3/account.cpp
+++ Project3/account.cpp
@@ -19,6 +19,27 @@ public:
 		balance -= money;
 	}
 
+	void increase5PercentInterest()
+	{
+		balance += (balance * 5 / 100);
+	}
+
+	void applyInterestForYears(int years)
+	{
+		for (int i = 0; i < years; i++)	applyInterest();
+	}
+
+	void applyInterest()
+	{
+		balance += (balance * interest / 100);
+	}
+
+	void setInterest(int rate)
+	{
+		interest = rate;
+	}
+
 private:
 	int balance;
+	int interest;
 };
